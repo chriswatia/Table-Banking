a6ca357fcad741fbbf21ab7c48d1485ef2f885de
chris watia <cwatia@seaburysolutions.com>
2020-05-07 16:55:41 +0300

    Implemted POST API

--- TableBanking/Controllers/UsersController.cs
+++ TableBanking/Controllers/UsersController.cs
@@ -24,5 +24,25 @@ namespace TableBanking.Controllers
         {
             return _tbankContext.Users.ToList();
         }
+
+        //Create User
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody]User user)
+        {
+            //check if user is supplied
+            if (user == null)
+            {
+                return NotFound("User data is not supplied");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _tbankContext.Users.AddAsync(user);
+            await _tbankContext.SaveChangesAsync();
+            return Ok(user);
+        }
+
     }
 }
